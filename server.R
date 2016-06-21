32441f7aebf5b7702386e5cfbf45e32ef1d8e904
Yu-Fu Wu <wennynnewenny@gmail.com>
2016-06-21 17:30:22 +0000

    cmuh / hello

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(datasets)
 Logged = FALSE;
-PASSWORD <- data.frame(Brukernavn = "withr", Password = "e10adc3949ba59abbe56e057f20f883e")
+PASSWORD <- data.frame(Brukernavn = "cmuh", Password = "5d41402abc4b2a76b9719d911017c592")
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
   source("www/Login.R",  local = TRUE)
